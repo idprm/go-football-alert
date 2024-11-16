04c9f7bd29927c2b1163deb97d55c9987d320c1d
Indra Pramana <idprama@gmail.com>
2024-11-17 00:36:56 +0700

    add new

--- internal/handler/incoming_handler.go
+++ internal/handler/incoming_handler.go
@@ -478,8 +478,6 @@ func (h *IncomingHandler) Detail(c *fiber.Ctx) error {
 	// setter msisdn
 	req.SetMsisdn(c.Get("User-MSISDN"))
 
-	log.Println(req.GetMsisdn())
-
 	// check if msisdn not found
 	if !req.IsMsisdn() {
 		return c.Status(fiber.StatusOK).SendString(h.MsisdnNotFound(c.BaseURL()))
@@ -521,6 +519,72 @@ func (h *IncomingHandler) Detail(c *fiber.Ctx) error {
 			)
 			replace := replacer.Replace(string(menu.GetTemplateXML()))
 			return c.Status(fiber.StatusOK).SendString(replace)
+		} else {
+
+			var data string
+
+			menu, err := h.menuService.GetBySlug(req.GetSlug())
+			if err != nil {
+				return c.Status(fiber.StatusBadGateway).SendString(err.Error())
+			}
+
+			if req.IsLmLiveMatch() {
+				data = h.LiveMatchs(c.BaseURL(), true, req.GetPage()+1)
+			}
+
+			if req.IsLmSchedule() {
+				data = h.Schedules(c.BaseURL(), req.GetPage()+1)
+			}
+
+			if req.IsFlashNews() {
+				data = h.FlashNews(c.BaseURL(), req.GetPage()+1)
+			}
+
+			if req.GetSlug() == "kit-foot" {
+				data = h.KitFoot(c.BaseURL(), req.GetPage()+1)
+			}
+
+			if req.GetSlug() == "kit-foot-by-league" {
+				data = h.KitFootByLeague(c.BaseURL(), req.GetLeagueId(), req.GetPage()+1)
+			}
+
+			if req.GetSlug() == "foot-europe" {
+				data = h.FootEurope(c.BaseURL(), req.GetPage()+1)
+			}
+
+			if req.GetSlug() == "foot-afrique" {
+				data = h.FootAfrique(c.BaseURL(), req.GetPage()+1)
+			}
+
+			if req.GetSlug() == "foot-international" {
+				// data = h.FootInternational(c.BaseURL(), req.GetPage()+1)
+				data = h.FootEurope(c.BaseURL(), req.GetPage()+1)
+			}
+
+			leagueId := strconv.Itoa(req.GetLeagueId())
+			teamId := strconv.Itoa(req.GetTeamId())
+			page := strconv.Itoa(req.GetPage() + 1)
+
+			paginate := `<a href="` + c.BaseURL() +
+				`/` + API_VERSION +
+				`/ussd/q?slug=` + req.GetSlug() +
+				`&amp;title=` + req.GetTitleQueryEscape() +
+				`&amp;league_id=` + leagueId +
+				`&amp;team_id=` + teamId +
+				`&amp;page=` + page +
+				`">Suiv</a>`
+
+			replacer := strings.NewReplacer(
+				"{{.url}}", c.BaseURL(),
+				"{{.version}}", API_VERSION,
+				"{{.date}}", utils.FormatFR(time.Now()),
+				"{{.data}}", data,
+				"{{.title}}", req.GetTitleWithoutAccents(),
+				"{{.paginate}}", paginate,
+				"&", "&amp;",
+			)
+			replace := replacer.Replace(string(menu.GetTemplateXML()))
+			return c.Status(fiber.StatusOK).SendString(replace)
 		}
 	}
 
@@ -556,6 +620,52 @@ func (h *IncomingHandler) Detail(c *fiber.Ctx) error {
 			)
 			replace := replacer.Replace(string(menu.GetTemplateXML()))
 			return c.Status(fiber.StatusOK).SendString(replace)
+		} else {
+
+			var data string
+
+			menu, err := h.menuService.GetBySlug(req.GetSlug())
+			if err != nil {
+				return c.Status(fiber.StatusBadGateway).SendString(err.Error())
+			}
+
+			if req.IsSMSAlerte() {
+				data = h.SMSAlerte(c.BaseURL(), req.GetPage()+1)
+			}
+
+			if req.GetSlug() == "sms-alerte" {
+				data = h.SMSAlerte(c.BaseURL(), req.GetPage()+1)
+			}
+
+			if req.GetSlug() == "sms-alerte-equipe" {
+				data = h.SMSAlerteEquipe(c.BaseURL(), req.GetPage()+1)
+			}
+
+			leagueId := strconv.Itoa(req.GetLeagueId())
+			teamId := strconv.Itoa(req.GetTeamId())
+			page := strconv.Itoa(req.GetPage() + 1)
+
+			paginate := `<a href="` + c.BaseURL() +
+				`/` + API_VERSION +
+				`/ussd/q?slug=` + req.GetSlug() +
+				`&amp;title=` + req.GetTitleQueryEscape() +
+				`&amp;league_id=` + leagueId +
+				`&amp;team_id=` + teamId +
+				`&amp;page=` + page +
+				`">Suiv</a>`
+
+			replacer := strings.NewReplacer(
+				"{{.url}}", c.BaseURL(),
+				"{{.version}}", API_VERSION,
+				"{{.date}}", utils.FormatFR(time.Now()),
+				"{{.data}}", data,
+				"{{.title}}", req.GetTitleWithoutAccents(),
+				"{{.paginate}}", paginate,
+				"&", "&amp;",
+			)
+			replace := replacer.Replace(string(menu.GetTemplateXML()))
+			return c.Status(fiber.StatusOK).SendString(replace)
+
 		}
 	}
 
